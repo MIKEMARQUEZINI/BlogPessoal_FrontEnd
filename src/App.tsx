340fd3dcc913aad93c7ceaeb2074730f977aaadc
Mike <marquezini.mike@outlook.com>
2023-06-14 17:45:13 -0300

    Implementacao NavBar, Home e Footer

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import Navbar from './pages/Components/static/navbar/Navbar'
+import Footer from './pages/Components/static/footer/Footer'
+import  Home  from './pages/Home/Home'
+
+function App() {
+
+  return (
+    <>  
+        <Navbar />
+        <Home />
+        <Footer />
+
+    </>
+  )
+} export default App
+
